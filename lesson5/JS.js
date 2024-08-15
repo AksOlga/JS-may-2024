1eebc6edef0f703ddf9387a2eb9c4ff30c4cc93a
AksOlga <aksenovao773@gmail.com>
2024-08-15 16:20:02 +0300

    Commit13

--- lesson5/JS.js
+++ lesson5/JS.js
@@ -1,123 +1,97 @@
+
 /*-------------------exercise-1----------------------*/
 
-
-// function square(a, b){
-//     return a * b}
-// square(15, 30);
-// let result = square(15 , 30);
-// console.log(result)
+// const square = (a, b) => a * b;
+// console.log(square(15, 30));
 
 /*-------------------exercise-2----------------------*/
-//
-// function square(r) {
-//     let S;
-//     S = Math.PI * Math.pow(r, 2);
-//     return S;
-// }
-//
-//     let result =square(5)
-//     console.log(result);
-
+// const square = (r) => Math.PI * Math.pow(r, 2);
+// console.log(square(5));
 
 /*-------------------exercise-3----------------------*/
 
-
-
-// function square(r, h){
-//     let S;
-//     S = 2 * Math.PI * r * h + 2 * Math.PI * Math.pow(r, 2);
-//     return S;
-// }
-// let result = square(5, 6 )
-// console.log(result)
+// const square = (r, h) => 2 * Math.PI * r * h + 2 * Math.PI * Math.pow(r, 2);
+// console.log(square(5,6))
 
 /*-------------------exercise-4----------------------*/
 
-//
-// function element(arr){
-//     for (const element of arr) {
-//         console.log(element);
+// //
+// const element=(arr) => {
+//     for (const el of arr) {
+//         console.log(el);
 //     }
-// }
+// };
 // let myArr =[3, 4, 5, 6]
-// element(myArr)
+// element(myArr);
 
 /*-------------------exercise-5----------------------*/
 
-// function paragraph(text) {
-//     document.write('<p>' + text + '</p>')
-//     }
-//
-// paragraph('Lorem ipsum dolor sit amet.')
+// const paragraph = (text) => '<p>' + text + '</p>';
+// document.write(paragraph('Lorem ipsum dolor sit amet.'));
 
 /*-------------------exercise-6----------------------*/
 
-// function paragraph(text) {
-//     document.write(
+// const paragraph = (text) =>
 //         '<ul>' +
-//             '<li>' + text + '</li>'+
-//             '<li>' + text + '</li>'+
-//             '<li>' + text + '</li>'+
-//         '</ul>')
-// }
-// paragraph('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, repellat.');
-
+//         '<li>' + text + '</li>'+
+//         '<li>' + text + '</li>'+
+//         '<li>' + text + '</li>'+
+//         '</ul>';
+// document.write(paragraph('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, repellat.'))
 
 
 /*-------------------exercise-7----------------------*/
 
-
-
-// function paragraph(text,counter) {
 //
-//         document.write('<ul>')
-//         for (let i = 0; i < counter; i++) {
-//             document.write(`<li>${text}</li>`);
-//         }
+// const paragraph = (text, counter) => {
+//     let result = '<ul>';
+//     for (let i = 0; i < counter; i++) {
+//         result += `<li>${text}</li>`;
+//     }
+//     result += '</ul>';
+//     return result;
+// };
 //
-//         document.write('</ul>');
-// }
-// paragraph( 'Lorem ipsum', 4)
+// document.write(paragraph('Lorem ipsum', 4));
 
 
 /*-------------------exercise-8----------------------*/
 
+
+// const foobar = (element) => {
 //
-// function foobar (element){
-//
-//     document.write('<ul>')
-//    for (const item of element){
-//        document.write(`<li>${item}</li>`)
-//    }
-//    document.write('</ul>');
-// }
-//
-// foobar([10, 20, true, 'lorem', 40, 50]);
+//     let result = '<ul>';
+//     for (const item of element) {
+//         result += `<li>${item}</li>`;
+//     }
+//     result += '</ul>';
+//     return result
+// };
+//     document.write(foobar([10, 20, true, 'lorem', 40, 50]))
 
 
 /*-------------------exercise-9----------------------*/
 
-//
-// function firm(users) {
+// const firm = (users) => {
+//     let result=''
 //     for (const user of users) {
-//         document.write(`<div>${user.name} ${user.id} ${user.age} </div>`);
+//          result +=`<div>${user.name} ${user.id} ${user.age} </div>`;
 //
 //     }
+//     return result
 // }
-//
-// firm([
+// document.write(firm([
 //     {id:1, name:'Vova', age:35},
 //     {id:2, name:'Vova', age:35},
 //     {id:3, name:'Vova', age:35},
 //     {id:5, name:'Vova', age:35},
 //
-// ]);
-
+// ]))
 
 /*-------------------exercise-10----------------------*/
 
-//
-// function minNum(nums) {
+
+// const minNum = (nums) =>{
 //     let min = nums[0];
 //     for (let i = 1; i < nums.length; i++) {
 //         let num = nums[i];
@@ -132,8 +106,8 @@
 
 /*-------------------exercise-11----------------------*/
 
-
-// function sum(arr){
+//
+// const sum=(arr)=>{
 //     let basket = 0;
 //     for (const arrKey of arr) {
 //         basket = basket + arrKey;
@@ -143,12 +117,11 @@
 //     }
 //
 //     console.log(sum([4, 5, 20]));
-//
 
 
 /*-------------------exercise-12----------------------*/
 //
-// function swap(arr, index1, index2) {
+// const swap=(arr, index1, index2) =>{
 //     let temp = arr[index1];
 //     arr[index1] = arr[index2];
 //     arr[index2] = temp;
@@ -157,9 +130,10 @@
 // }
 // console.log(swap([11, 22, 33, 44], 0, 1));
 
+
 /*-------------------exercise-13----------------------*/
 
-function exchange(sumUAH, currencyValues, exchangeCurrency) {
+const exchange = (sumUAH, currencyValues, exchangeCurrency) => {
     for (let i = 0; i < currencyValues.length; i++) {
         if (currencyValues[i].currency === exchangeCurrency) {
             let rate = currencyValues[i].value;
